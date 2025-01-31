7111d14fdf0bca489ed0295fe0d773565040c198
Umaid <umaidimtiyaz6@gmail.com>
2025-01-31 23:42:48 +0530

    Changed footer name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
 
 
             </div>
-            <div className='flex justify-center items-center'> Created with <img className='w-7 mx-2' src="icons/heart.png" alt="" /> by CodeWithHarry </div>
+            <div className='flex justify-center items-center'> Created with <img className='w-7 mx-2' src="icons/heart.png" alt="" /> by CodeWithUmaid </div>
         </div>
     )
 }
