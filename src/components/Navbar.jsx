43bae7e29a28e3bc753449b49474a796ac7a7b08
lalith-codeable <docs.work.gooogle@gmail.com>
2025-01-31 21:50:30 +0530

    Initailized tailwindcss v4 -_-

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
     return (
         <nav className='bg-slate-800 text-white '>
-            <div className="mycontainer flex justify-between items-center px-4 py-5 h-14">
+            <div className="flex justify-between items-center px-4 py-5 h-14">
 
                 <div className="logo font-bold text-white text-2xl">
                     <span className='text-green-500'> &lt;</span>
